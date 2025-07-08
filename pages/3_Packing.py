fbe8d95aa62a851fcf0c072b21beb4ac988deb0a
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 15:42:30 +0700

    Update 3_Packing.py

--- pages/3_Packing.py
+++ pages/3_Packing.py
@@ -2,5 +2,25 @@ import streamlit as st
 
 st.set_page_config(page_title="Packing", layout="wide")
 st.title("Packing")
+pic_list = [
+    "Rikie Dwi Permana",
+    "Idha Akhmad Sucahyo",
+    "Rian Dinata",
+    "Harimurti Krisandki",
+    "Muchamad Mustofa",
+    "Yogie Arie Wibowo"
+]
+db_list = [
+    "DMI",
+    "PKS",
+    "PMT",
+    "PSM",
+    "PSS",
+    "PST"
+]
+selected_pic = st.selectbox("PIC (Submitting this form):", pic_list)
+selected_db = st.selectbox("Database:", db_list)
+nomor_pl = st.number_input("Nomor Pick List:")
+
 
 st.write("This is a placeholder for the Packing module.")
