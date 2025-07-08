2b34ab4c913dacfc04372f740b1c0399393b13d2
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 11:00:10 +0700

    Update and rename Delivery Plan.py to 4_Delivery Plan.py

---
+++ pages/4_Delivery Plan.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Delivery Planning", layout="wide")
+st.title("Delivery Planning")
+
+st.write("This is a placeholder for the Delivery Planning module.")
