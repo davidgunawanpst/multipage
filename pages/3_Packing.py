a12b8a5c721cda3b1399482d0f0cbe4cbc91546e
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 11:02:16 +0700

    Update and rename Packing.py to 3_Packing.py

---
+++ pages/3_Packing.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Packing", layout="wide")
+st.title("Packing")
+
+st.write("This is a placeholder for the Packing module.")
--- pages/Packing.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.set_page_config(page_title="Pick & Pack", layout="wide")
-st.title("Pick & Pack")
-st.write("This is a placeholder for the Pick & Pack module.")
