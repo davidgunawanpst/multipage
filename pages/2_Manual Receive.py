309fe04d226b7320d85f31e2a0cd3411aaf11dd4
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 11:01:49 +0700

    Update and rename Manual Receive.py to 2_Manual Receive.py

--- pages/2_Manual Receive.py
+++ pages/2_Manual Receive.py
@@ -1,4 +1,6 @@
 import streamlit as st
+
 st.set_page_config(page_title="Manual Receive", layout="wide")
-st.title("📝 Manual Receive")
+st.title("Manual Receive")
+
 st.write("This is a placeholder for the Manual Receive module.")
