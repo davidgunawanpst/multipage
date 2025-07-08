11333350b21349e1a77ff68df6af2a17e0ad7d5b
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-08 16:26:17 +0700

    Update 4_Delivery Plan.py

--- pages/4_Delivery Plan.py
+++ pages/4_Delivery Plan.py
@@ -4,3 +4,29 @@ st.set_page_config(page_title="Delivery Planning", layout="wide")
 st.title("Delivery Planning")
 
 st.write("This is a placeholder for the Delivery Planning module.")
+pic_list = [
+  "Sendy Juniar Eka",
+  "Junedi",
+  "Abim Priambada"
+]
+db_list = [
+    "DMI",
+    "PKS",
+    "PMT",
+    "PSM",
+    "PSS",
+    "PST"
+]
+pl_list = [
+  11111,
+  22222,
+  33333,
+  44444
+]
+
+selected_pic = st.selectbox("PIC (Submitting this form):", pic_list)
+selected_db = st.selectbox("Database:", db_list)
+tujuan = st.text_input("Tujuan Pengiriman:")
+nomor_pl = st.multiselect("Nomor Pick List Terkirim:", pl_list)
+
+st.write("This is a placeholder for the Packing module.")
