879a4df3ce03151e700943db59c5fc4f76c0bfa1
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-10 11:52:14 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 st.set_page_config(page_title="Warehouse Operations Suite", layout="wide")
-st.title("📦 Welcome to the Warehouse App")
-st.write("Use the sidebar to navigate through different warehouse modules.")
+st.title("Welcome to the Warehouse App")
+st.write("Silahkan gunakan sidebar untuk menavigasi menu yang dituju.")
+st.write("Untuk monitoring detail per item dapat diakses di link berikut:")
+
+st.markdown("[Monitoring Detail WMA](https://docs.google.com/spreadsheets/d/1K3_X8Dkx_fyulNSTIvMk0wR_Xwky14Yh5MVfGDAipA8)")
