116c259a94b947e94eb680997213c3a8cf536f0d
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-14 17:07:40 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,4 +8,4 @@ if check_password():
     st.title("Welcome to the Warehouse App")
     st.write("Silahkan gunakan sidebar untuk menavigasi menu yang dituju.")
     st.write("Untuk monitoring detail per item dapat diakses di link berikut:")
-    st.markdown("[Monitoring Detail WMA](https://docs.google.com/spreadsheets/d/1K3_X8Dkx_fyulNSTIvMk0wR_Xwky14Yh5MVfGDAipA8)")
+st.markdown("[Monitoring Detail WMA](https://docs.google.com/spreadsheets/d/1K3_X8Dkx_fyulNSTIvMk0wR_Xwky14Yh5MVfGDAipA8)")
