43244fff15cea7406468fa4fc6c795e80bcd8c2f
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-07-14 17:07:14 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
-st.set_page_config(page_title="Warehouse Operations Suite", layout="wide")
-st.title("Welcome to the Warehouse App")
-st.write("Silahkan gunakan sidebar untuk menavigasi menu yang dituju.")
-st.write("Untuk monitoring detail per item dapat diakses di link berikut:")
+import streamlit as st
+from auth import check_password
 
-st.markdown("[Monitoring Detail WMA](https://docs.google.com/spreadsheets/d/1K3_X8Dkx_fyulNSTIvMk0wR_Xwky14Yh5MVfGDAipA8)")
+if check_password():
+    st.set_page_config(page_title="Warehouse Operations Suite", layout="wide")
+    st.title("Welcome to the Warehouse App")
+    st.write("Silahkan gunakan sidebar untuk menavigasi menu yang dituju.")
+    st.write("Untuk monitoring detail per item dapat diakses di link berikut:")
+    st.markdown("[Monitoring Detail WMA](https://docs.google.com/spreadsheets/d/1K3_X8Dkx_fyulNSTIvMk0wR_Xwky14Yh5MVfGDAipA8)")
