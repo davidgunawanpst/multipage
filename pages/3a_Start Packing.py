f9a2c9e1867b1bec4cf5a56727d3a4207e325805
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-10-22 11:52:03 +0700

    Update 3a_Start Packing.py

--- pages/3a_Start Packing.py
+++ pages/3a_Start Packing.py
@@ -8,7 +8,7 @@ from auth import check_password  # keep your auth
 st.set_page_config(page_title="Packing Start", layout="wide")
 
 # --- WEBHOOK URL ---
-WEBHOOK_URL_DATA = "[INSERT WEBHOOK URL]"
+WEBHOOK_URL_DATA = "https://script.google.com/macros/s/AKfycby271wV3KRfgiPIQ_XkHaJSUXjIBF9ftCJ_MmPw6iNXmAXlU94YN3HSAjnl23nmg8A3ig/exec"
 
 # --- Static lists ---
 pic_list = [
