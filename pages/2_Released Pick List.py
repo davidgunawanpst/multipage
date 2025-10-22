070bc4db09cc47a8c89c5ab074e4a49cf34262a1
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-10-22 13:33:52 +0700

    Update 2_Released Pick List.py

--- pages/2_Released Pick List.py
+++ pages/2_Released Pick List.py
@@ -50,7 +50,7 @@ if check_password():
                 "timestamp": timestamp,
                 "pic": selected_pic,
                 "database": selected_db,
-                "nomor": pick_number.strip(),
+                "pl_released": pick_number.strip(),
             }
 
             try:
