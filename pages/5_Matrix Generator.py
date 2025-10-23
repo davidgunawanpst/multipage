13e2e2f9192cd0c9411a4442cb0d6c6b636fe1e9
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-10-23 17:36:59 +0700

    Update 5_Matrix Generator.py

--- pages/5_Matrix Generator.py
+++ pages/5_Matrix Generator.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Approval", layout="wide")
-st.title("Approval")
+st.set_page_config(page_title="Matrix Generator", layout="wide")
+st.title("Matrix Generator")
 
-st.write("This is a placeholder for the Approval module.")
+st.write("This is a placeholder for the Matrix Generator module.")
