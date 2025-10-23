82e70e2a25c9dbf1935a68339b8a086bc48d3776
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-10-23 17:39:20 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,5 +7,5 @@ if check_password():
     st.set_page_config(page_title="Warehouse Operations Suite", layout="wide")
     st.title("Welcome to the Packing Monitoring App (sementara)")
     st.write("Silahkan gunakan sidebar untuk menavigasi menu yang dituju.")
-    st.write("Untuk monitoring detail per item dapat diakses di link berikut:")
+    st.write("Untuk monitoring Pick List dapat diakses di link berikut:")
     st.markdown("[Monitoring SLA Pick List](https://docs.google.com/spreadsheets/d/1YsSJSlezQHZKdY0P21Co7NxecPzrmYNCKMvbceYaLEo)")
