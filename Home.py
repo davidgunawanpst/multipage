7ea287ed62a237175241ca029833417958921b8c
davidgunawanpst <david.gunawan@pancaran-shipping.co.id>
2025-11-17 22:46:02 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ from auth import check_password
 
 if check_password():
     st.set_page_config(page_title="Warehouse Operations Suite", layout="wide")
-    st.title("Welcome to the Marunda Warehouse Management System")
+    st.title("Welcome to Marunda Warehouse Management System")
     st.write("Silahkan gunakan sidebar untuk menavigasi menu yang dituju.")
     st.write("Untuk monitoring Pick List dapat diakses di link berikut:")
     st.markdown("[Monitoring SLA Pick List](https://docs.google.com/spreadsheets/d/1YsSJSlezQHZKdY0P21Co7NxecPzrmYNCKMvbceYaLEo)")
